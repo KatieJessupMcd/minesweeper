06ec5c042c61253c6ba7e6512e0f62a6e848a1c4
KatieJessupMcD <katie.jessupmcd@gmail.com>
2019-03-07 15:51:19 -0800

    set up Cell Component boilerplate

--- src/Cell.js
+++ src/Cell.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 // import './Cell.css';
+import PropTypes from 'prop-types'
 
 class Cell extends Component {
-  static defaultProps = {};
+  constructor(props){
+    super(props)
+    this.state = {
+      isRevealed: false, 
+      value: -1 | 0 | 1, 
+      isFlagged: true, 
+      xCoord: 0, 
+      yCoord: 0
+    }
+    this.revealValue = this.revealValue.bind(this); 
+  }
+
+  revealValue(){
+    // function that will do a depth first search to find if the one clicked on is a bomb
+    // and check for the next door neighboor values
+  }
   render() {
     return <div className='Cell'><p>This is a cell</p></div>;
   }
 }
+Cell.defaultProps = {}
+Cell.propTypes = {}
 
 export default Cell;
